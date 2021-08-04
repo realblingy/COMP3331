19c89e534683544f014b7d7408c23fe5f57c0fd3
James Dang <jamesphidang@gmail.com>
2021-08-04 18:44:19 +1000

    May be complete with sliding window

--- sender.py
+++ sender.py
@@ -76,15 +76,14 @@ if segment['syn'] == 1 and segment['ack'] == 1:
     packetLoss = False
     # Sequence number of loss packet
     packetLossSequence = None
-    # Starting index of window
+    # Start pointer of window
     windowStart = 0
-    # Number of packets sent or dropped
-    windowSize = 0
+    # End pointer of window
+    windowEnd = min(MWS, len(segmentsToSend))
 
     # Sends PTP segments with data
     while segmentsToSendIndex < len(segmentsToSend):
         segmentPayload = segmentsToSend[segmentsToSendIndex]
-        
         PTPsegement = createSegement(
             sequenceNumber,
             acknowledgementNumber,
@@ -94,74 +93,30 @@ if segment['syn'] == 1 and segment['ack'] == 1:
 
         # PL modules
         if (random.random() > pdrop): 
+            print("Sending")
             senderSocket.sendto(PTPsegement, (receiverIP, receiverPort))
-            print("Sent segment")
-            
-
             if packetLoss == False:
                 windowStart += 1
+                packetLossSequence = sequenceNumber
+                windowEnd = min(windowEnd + 1, len(segmentsToSend))
         else:
-            print("Dropped!")
+            print("Dropping")
             if packetLoss == False:
-                windowStart = segmentsToSendIndex
                 packetLossSequence = sequenceNumber
                 packetLoss = True
 
+        sequenceNumber += len(segmentPayload)
         print(PTPsegement)
         print()
-        sequenceNumber += len(segmentPayload)
-        windowSize += 1
-        segmentsToSendIndex += 1
 
-        # If we reached the maximum window size, we wait for all ACKs
-        if windowSize == MWS:
-            receivedAcks = 0
-            while receivedAcks < MWS:
-                # Checks if any packets are lost
-                try:
-                    message, senderAddress = senderSocket.recvfrom(2048)
-
-                    ackSegment = json.loads(message.decode('utf-8'))
-                    print("Received segemnet")
-                    print(ackSegment)
-                    print()
-
-                    if ackSegment['ack'] == 1:
-                        receivedAcks += 1
-                # If packet is lost, revert back to the first unAcked segment
-                except:
-                    packetLoss = False
-                    sequenceNumber = packetLossSequence
-                    segmentsToSendIndex = windowStart
-                    break
-                windowSize = 0 
-        elif segmentsToSendIndex == len(segmentsToSend):
-            receivedAcks = 0
-            while receivedAcks < segmentsToSendIndex % MWS:
-                try:
-                    message, senderAddress = senderSocket.recvfrom(2048)
-
-                    ackSegment = json.loads(message.decode('utf-8'))
-                    print("Received segemnet")
-                    print(ackSegment)
-                    print()
-
-                    if ackSegment['ack'] == 1:
-                        receivedAcks += 1
-                # If packet is lost, revert back to the first unAcked segment
-                except:
-                    packetLoss = False
-                    sequenceNumber = packetLossSequence
-                    segmentsToSendIndex = windowStart
-                    break
-                windowSize = 0 
-
-        #         if ackSegment['ack'] == 1:
-        #             receivedAcks += 1;
-                
-        #         windowSize = 0
-
-# Closing connection
+        try:
+            message, senderAddress = senderSocket.recvfrom(2048)
+            segmentsToSendIndex += 1
+            sequenceNumber += len(payload)
+        except:
+            segmentsToSendIndex = windowStart
+            packetLoss = False
+            sequenceNumber = packetLossSequence
 
 finSegment = createSegement(
     sequenceNumber,
